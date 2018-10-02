08997404970927626740e4f54588277568c884a3
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-10-02 09:51:00 -0500

    writing better functions

--- week1/javascriptFunctions/client.js
+++ week1/javascriptFunctions/client.js
@@ -15,16 +15,16 @@ console.log(`The number is ${number}`);
 
 // Function above isn't self contained - works, but could be better
 
+// Use an input parameter to avoid using outside variables without being obvious 
 
-function betterDoubleIt(num){
-  console.log('Number passed in is', number);
-  number = 0;
-  console.log('In the function number is', number);
-  
+function betterDoubleIt(num){ 
+  return num * 2; // return back the result don't set outside variables 
 }
 
-betterDoubleIt(number)
-console.log('After the function Number is now ', number);
-
+let result = betterDoubleIt(number);
+console.log('After the function Number is now ', result);
 
 // why is this better: 
+// - self contained, doesn't change anything outside
+// - easier to read and understand
+// - use it elsewhere, just move function definition 
