43bbdcccafb3a4298acf5b7b138c05e1e8ea9e16
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-10-02 10:03:24 -0500

    end of function review

--- week1/javascriptFunctions/client.js
+++ week1/javascriptFunctions/client.js
@@ -15,6 +15,9 @@ console.log(`The number is ${number}`);
 
 // Function above isn't self contained - works, but could be better
 
+// Can use the function before it's declared in the file 
+// console.log('Use betterDoubleIt on 2', anotherDoubleIt(2));
+
 // Use an input parameter to avoid using outside variables without being obvious 
 
 function betterDoubleIt(num){ 
@@ -28,3 +31,23 @@ console.log('After the function Number is now ', result);
 // - self contained, doesn't change anything outside
 // - easier to read and understand
 // - use it elsewhere, just move function definition 
+
+// another way to define a function 
+// This is called a function expression 
+let anotherDoubleIt = function(number){
+  return number * 2;
+}
+
+
+// This isn't going to work, dind't source in jQuery
+$('#myButton').on('click', function(){
+  // do something 
+})
+
+function handleClick(){
+  // do something 
+}
+
+// Use a named function for an event 
+// Don't put the ()'s here or it will call the function 
+$('#myButton').on('click', handleClick)
