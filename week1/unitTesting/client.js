cacd57d7ad47353733e7f9d3373768d9dae7dabb
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-10-02 14:03:58 -0500

    Added readme for tests reference and started testing in js

--- week1/unitTesting/client.js
+++ week1/unitTesting/client.js
@@ -1,2 +1,25 @@
-console.log('js');
+console.log('Function to sum 2 things');
+
+function sum(value1, value2 = 0) {
+    let number1 = parseFloat(value1);
+    let number2 = parseFloat(value2);
+    return number1 + number2;
+}
+//say what you're testing and expectations to see if they match in the console
+console.log(`Try sum(2, 3) expect it to give us 5:`, sum(2,3));
+console.log(`Try sum(2, -3) expect it to give us -1:`, sum(2,-3));
+
+//broke the thought in readme so what can we do to fix it? 
+console.log(`Try sum('2', '3') expect it to give us 5:`, sum('2','3'));
+//    let number1 = parseInt(value1);
+//    let number2 = parseInt(value2);
+//    return number1 + number2;
+
+// fixed with parseFloat
+console.log(`Try sum(2.5, -1) expect it to give us 1.5:`, sum(2.5,-1));
+
+console.log(`Try sum('foo', 'bar') expect it to give us NaN:`, sum('foo','bar'));
+
+// changed value2 to value2 = 0)
+console.log(`Try sum(3) expect it to give us 3:`, sum(3));
 
---
+++ week1/unitTesting/readme.md
@@ -0,0 +1,18 @@
+Testing File Example: 
+
+Sum
+take some input 2 numbers
+return back the 2 numbers added together
+
+Test with: 
+sum(2, 3) expect it to give us 5
+sum(2, -2) expect it to give -1
+sum('2', '3') expect it to give us 5
+sum(2.5, -1) expect 1.5
+sum('foo', 'bar') expect it to give NaN
+sum(3) expect it to treat the other as 0 and give 3
+sum(2, 3, 5) expect to ignore anything extra, add two only
+sum(true, false) expect it to give NaN
+sum(5/4, 67) -same test case as sum(2, 3)
+let num1 = something, num2 = something
+also are the same as above but depends on what num1 is & num2 is
