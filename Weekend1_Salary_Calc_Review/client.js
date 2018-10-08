5b6aa462721f97ebf5b665664e392f5904963abf
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-10-08 10:58:47 -0500

    most of app written out

--- Weekend1_Salary_Calc_Review/client.js
+++ Weekend1_Salary_Calc_Review/client.js
@@ -1,5 +1,7 @@
 console.log('js sourced');
 
+let totalYearlySalary = 0;
+
 $(document).ready(handleReady);
 
 //This doesn't work
@@ -7,5 +9,59 @@ $(document).ready(handleReady);
 
 function handleReady() {
     console.log('jquery sourced');
-    
+    addClickListeners();
+}
+
+//When submit is clicked get info from inputs
+
+function addClickListeners() {
+    $('#submitBtn').on('click', handleSubmit);
+}
+
+function handleSubmit() {
+    console.log('submit Clicked!');
+    //grab input data .val() 
+    let firstName = $('#firstName').val();
+    let lastName = $('#lastName').val();
+    let idNumber = $('#idNumber').val();
+    let title = $('#title').val();
+    let salary = $('#salary').val();
+
+    // push amounth of salary to total annual salary
+    totalYearlySalary += parseInt(salary);
+
+    console.log(totalYearlySalary);
+
+    calculateAndAppendMonthlySalary();
+
+    console.log('Here is your data:', firstName, lastName, idNumber, title, salary);
+
+    // take data, put into table
+    // "<td>" + firstName + "</td>"
+
+    let stringOfHtml = `
+    <tr>
+      <td>${firstName}</td>
+      <td>${lastName}</td>
+      <td>${idNumber}</td>
+      <td>${title}</td>
+      <td>${salary}</td>
+    </tr>
+    `;
+
+    $('#tableContent').append(stringOfHtml);
+
+    //To Do Labels
+    // To Do Clear Inputs
+
+}
+
+function calculateAndAppendMonthlySalary() {
+    let monthlyAmount = totalYearlySalary / 12;
+
+    $('#total').empty();
+
+    //append to DOM
+    $('#total').append(`<p> Total Monthly Salaries: ${monthlyAmount}</p>`);
+
 }
--- week1/jqueryTricks/client.js
+++ week1/jqueryTricks/client.js
@@ -1,8 +1,80 @@
 console.log('js');
 
+let moviesArray = [];
+
+class Movie {
+    constructor(title, year, director){
+        this.title = title;
+        this.year = year;
+        this.director = director;
+    }
+}
+
 $('document').ready(onReady);
 
 function onReady(){
     console.log('JQ');
+    $('#addNewMovie').on('click', addMovie);
+    //find something already on the page with movieList id
+    $('#movieList').on('click', '.lendMovie', lendMovie);
+
+    // . = class and class is best to use since it can only be used once 
+    $('#movieList').on('click', '.deleteMovie', deleteMovie);
+}
+
+// add movie function
+
+function addMovie() {
+    // event.preventDefault adds to console and prevents repeats
+    event.preventDefault();
+    console.log('button clicked!');
+    let titleIn = $('#title').val();
+    let yearIn = $('#year').val();
+    let directorIn = $('#director').val();
+    // new instance of class using information from the form 
+    let newMovie = new Movie(titleIn, yearIn, directorIn);
+    moviesArray.push(newMovie);
+    console.log(moviesArray);
+
+    //call function to append movie list
+    appendMovieList();
+}
+
+// appending!
+
+function appendMovieList() {
+    let element = $('#movieList');
+    element.empty();
+    for(let movie of moviesArray) {
+        console.log(movie);
+        element.append(`<li>` + movie.title + ` ` + movie.year + ` ` + movie.director + 
+        `<button class="lendMovie">Lend</button>
+        <button class="deleteMovie">Delete</button></liv>`);
     
+
+    }
+}
+
+// lending!
+
+function lendMovie(){
+    console.log('lend clicked!');
+    //this is from the button being clicked 
+    $(this).parent().toggleClass('yellow');
+    
+}
+
+// delete!
+
+function deleteMovie() {
+    console.log('delete!');
+    let selectedItem = $(this).parent().text();
+    console.log(selectedItem);
+    for(let i = 0; i < moviesArray.length; i++){
+        if(selectedItem.includes(moviesArray[i].title)){
+            console.log('delete me!');
+            moviesArray.splice(i, 1);
+            $(this).parent().remove();
+        }
+    }
 }
