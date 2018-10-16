abdc20d48e05b5bd637c55dd1894a5ffa6a0484d
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-10-16 14:46:34 -0500

    set up get to get information from server

--- week3/pgIntro/server/server.js
+++ week3/pgIntro/server/server.js
@@ -33,6 +33,26 @@ app.use(bodyParser.json( ) );
 // Static files
 app.use( express.static('public/static') );
 
+//set GET route to get all songs from database
+app.get('/songs', (req, res) => {
+    // When you fetch all things in these GET routes, storngly 
+    //encourage ORDER BY
+    // so that things always come back in a consistent order 
+    const sqlText = `SELECT * FROM songs ORDER BY published, track, artist DESC;`;
+    pool.query(sqlText)
+        .then ((result) => {
+            console.log(`Got stuff back from the database`, result);
+            res.send(result.rows);
+            
+        }).catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500); // Good server always respondeds
+            
+        });
+});
+
+//set up POST route to add a new song to the database
+
 const port = process.env.PORT || 5000;
 app.listen( port, () => {
     console.log('up and running on port', port);
